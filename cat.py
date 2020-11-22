f6455e15dfc0701aa2fbfc011cca288d61bae23c
Vallna <valentin4688@gmail.com>
2020-11-22 23:24:37 +0200

    Change file

---
+++ cat.py
@@ -0,0 +1,14 @@
+class Cat:
+    def __init__(self, name, gender, age):
+        self.name = name
+        self.gender = gender
+        self.age = age
+    # def catName(self):
+    #     return self.name
+    #
+    # def catGender(self):
+    #     return self.gender
+    #
+    # def catAge(self):
+    #     return self.age
+
